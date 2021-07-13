abb7b1cdf49324010646a39510541a41ca6c38d8
Tayyab <heyitstay011@gmail.com>
2021-07-13 15:33:09 +0530

    Clickabl divs to btn

--- Carousel/app.js
+++ Carousel/app.js
@@ -1,6 +1,6 @@
 const startSlider = (slider) => {
-    const rightArrow = slider.querySelector('.arrow.right'); // next
-    const leftArrow = slider.querySelector('.arrow.left'); // previous
+    const rightArrow = slider.querySelector('.arrow.right'); // next button
+    const leftArrow = slider.querySelector('.arrow.left'); // previous button
     const cards = slider.querySelectorAll('.card'); // All cards inside slider
     let index = 0; // initial index
     const cleanUpCardClasses = () => {
@@ -9,17 +9,17 @@ const startSlider = (slider) => {
     const updateCardClasses = () => {
         cleanUpCardClasses(); 
         cards[index].classList.add('active');
-        (cards[index + 1] && cards[index + 1].classList.add('next')); // if next card exists add next class
-        if(index === cards.length - 1 )  cards[0].classList.add('next'); // if last card is active add next to first card
-        (cards[index - 1] && cards[index - 1].classList.add('prev')); // if prev card exists add prev class
-        if(index === 0 )  cards[cards.length - 1 ].classList.add('prev'); // if first card is active add prev to last card
+            // if last card is active add next class to first card
+        index === cards.length - 1  ? cards[0].classList.add('next') : cards[index + 1].classList.add('next'); 
+            // if first card is active add prev class to last card  
+        index === 0 ? cards[cards.length - 1 ].classList.add('prev') : cards[index - 1].classList.add('prev');  
     }
     rightArrow.addEventListener('click', () => {
-        index = index === (cards.length - 1) ? 0 : index + 1;
+        index = index === (cards.length - 1) ? 0 : index + 1; // Increment index within limits
         updateCardClasses();
     });
     leftArrow.addEventListener('click', () => {
-        index = index === 0 ? (cards.length - 1) : index - 1;
+        index = index === 0 ? (cards.length - 1) : index - 1; // Decrement index within limits
         updateCardClasses();
     });
 }
